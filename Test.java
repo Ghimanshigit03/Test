edd9009ee3d1e310b02fde3b6b5a0c0a976a6ab3
Himanshi_Ga <ghimanshi2508@gmail.com>
2025-07-24 08:18:05 +0000

    Modified some files

--- Test.java
+++ Test.java
@@ -1,1 +1,16 @@
-//this is a file for writing test cases
+//function to calcualte the sum of two numbers
+public class Test {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = add(a, b);
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
+
+//number of times a particular date appears on friday between 2 dates
